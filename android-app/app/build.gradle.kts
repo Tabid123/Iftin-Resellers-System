099e4b89a1c4d1f13df58d432dfa5bb372cc6bb4
Tabid123 <saabiraxmedmaxamed10@Gmail.com>
2026-09-20 18:57:10 +0300

    Fix device-bound discovery RPC calls
    * Send device id to hardened discovery RPCs
    
    * Use device-bound discovery RPC client calls
    
    * Bump delivery agent to v3.3 for discovery RPC fix

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.iftin.resellers"
         minSdk = 23
         targetSdk = 34
-        versionCode = 14
-        versionName = "3.2"
+        versionCode = 15
+        versionName = "3.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
