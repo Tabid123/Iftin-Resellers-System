2157392b88ab96c676edba3a7940de14670bf25c
Tabid123 <saabiraxmedmaxamed10@Gmail.com>
2026-09-26 13:57:39 +0300

    Stop second WebView reload during tenant APK startup

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -43,7 +43,6 @@ import { useQueryClient } from "@tanstack/react-query";
 import appCss from "../styles.css?url";
 import { reportLovableError } from "../lib/lovable-error-reporting";
 import { registerServiceWorker } from "@/lib/registerServiceWorker";
-import { openLiveTenantWhenAvailable } from "@/lib/liveNativeUpdates";
 
 const CHUNK_RELOAD_KEY = "iftin:chunk-reload";
 const OFFLINE_CACHE_SCHEMA_KEY = "iftin:offline-cache-schema";
@@ -223,10 +222,6 @@ function AppContent() {
 
 function RootComponent() {
   const { queryClient } = Route.useRouteContext();
-
-  useEffect(() => {
-    void openLiveTenantWhenAvailable();
-  }, []);
 
   useEffect(() => {
     registerServiceWorker();
